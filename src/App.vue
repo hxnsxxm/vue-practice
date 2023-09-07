9707c5ce9972208401283790750d4af047c1b777
hansaem <hansaemj20@gmail.com>
2023-09-07 21:36:15 +0900

    3. v-for 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div>
-      원룸샵
-      <h4>XX 원룸</h4>
-      <p>{{ price1 }} 만원</p>
+    <div class="menu">
+      <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
     </div>
-    <div>
-      <h4>XX 원룸</h4>
-      <p>{{ price2 }} 만원</p>
+    <div v-for="(info, i) in infos" :key="i">
+      <h4>{{ info.product }}</h4>
+      <p>{{ info.price }} 만원</p>
     </div>
   </div>
 </template>
@@ -19,8 +16,12 @@ export default {
   name: 'App',
   data(){
     return {
-      price1 : 60,
-      price2 : 70,
+      menus : ['Home', 'Shop', 'About'],
+      infos : [
+        {product: '역삼동원룸', price: '50'},
+        {product: '천호동원룸', price: '60'},
+        {product: '마포구원룸', price: '70'},
+      ]
     }
   },
   components: {
@@ -34,7 +35,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
